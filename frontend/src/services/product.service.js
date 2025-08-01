2aaba822a030aedfd86e1f6bdb4faf65542b490a
francis.targanski <francis@targanski.com>
2025-08-01 14:55:32 -0300

    feat: atualizar variáveis de ambiente para Vite em vez de React

--- frontend/src/services/product.service.js
+++ frontend/src/services/product.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
 
 const getProducts = async () => {
   try {
