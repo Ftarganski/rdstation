5eab7b5f07bc12ddd779a301a81b79748dd9f16e
francis.targanski <francis@targanski.com>
2025-08-01 15:18:01 -0300

    fix: atualizar configuração do vercel.json e ajustar .vercelignore para exclusão de pastas desnecessárias

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -24,6 +25,7 @@ export default defineConfig({
   build: {
     outDir: 'build',
     sourcemap: true,
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks: {
